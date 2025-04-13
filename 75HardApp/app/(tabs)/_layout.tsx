b2d85d60a81a0e6336c52fe25b05258e3d9a72c1
MokshaSri <moksha.srinivas@gmail.com>
2025-04-12 23:52:24 -0400

    added initial tab

--- 75HardApp/app/(tabs)/_layout.tsx
+++ 75HardApp/app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
@@ -37,6 +36,13 @@ export default function TabLayout() {
         name="explore"
         options={{
           title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="seventyfive-hard"
+        options={{
+          title: 'seventyfive-hard',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
